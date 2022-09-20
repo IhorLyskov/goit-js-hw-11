8c4c8b0011a82950ae37f6368abeeb4e93c8580d
IhorLyskov <99328897+IhorLyskov@users.noreply.github.com>
2022-09-21 02:46:21 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import { renderPictures } from './js/markup';
 import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 const form = document.querySelector('.gallery__search-form');
 const galleryPictures = document.querySelector('.gallery__pictures');
 const loadMore = document.querySelector('.load-more');
@@ -15,6 +18,10 @@ let gQuery;
 let gPage = 1;
 let gTotalHits = 0;
 const PER_PAGE_MAX = 40;
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 async function searchPictures() {
   try {
@@ -26,14 +33,19 @@ async function searchPictures() {
         Notiflix.Notify.info(`Hooray! We found ${gTotalHits} images.`);
       }
       galleryPictures.innerHTML = renderPictures(pictures);
+      gallery.refresh();
       gPage += 1;
       gTotalHits -= pictures.hits.length;
-      if (gTotalHits) {
+      if (gTotalHits > 0) {
         loadMore.classList.remove('visually-hidden');
+      } else {
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
       }
     } else {
-      Notiflix.Notify.failure(
-        "We're sorry, but you've reached the end of search results."
+      Notiflix.Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
       );
     }
   } catch {
@@ -64,7 +76,7 @@ function onFormSubmit(evt) {
 loadMore.addEventListener('click', onLoadMoreForm);
 
 function onLoadMoreForm(evt) {
-  if (gTotalHits) {
+  if (gTotalHits > 0) {
     searchPictures();
   }
 }
